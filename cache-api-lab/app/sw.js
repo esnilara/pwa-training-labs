e7b3e9d50e710718d98aceede66a3cc54f693931
Esteban Lara <esteban@esnilara.io>
2019-06-17 19:37:53 -0500

    chore: cache-api-lab serve files from the cache

--- cache-api-lab/app/sw.js
+++ cache-api-lab/app/sw.js
@@ -18,3 +18,23 @@ self.addEventListener('install', event => {
     })
   );
 });
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request);
+
+      // TODO: Add fetched files to the cache.
+    }).catch(error => {
+      // TODO: Respond with custom offline page.
+    })
+  );
+});
